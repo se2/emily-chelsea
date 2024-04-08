db4e7973043df3074b703481e800ee206faa09dc
Trung Ngo <ngoquangtrung.it@gmail.com>
2024-04-08 21:32:43 +0700

    update code

--- wp-content/themes/emily-chelsea/inc/hooks/woo/woocommerce_dropdown_variation_attribute_options_args.php
+++ wp-content/themes/emily-chelsea/inc/hooks/woo/woocommerce_dropdown_variation_attribute_options_args.php
@@ -3,7 +3,8 @@ add_filter('woocommerce_dropdown_variation_attribute_options_args', function ($a
     $term = get_taxonomy($args['attribute']);
     if (!is_wp_error($term) && !empty($term)) {
         $label = str_replace('Product', "", $term->label);
-        $args['show_option_none'] = "Choose an " .  $label;
+        $args['show_option_none'] = "Choose your " .  $label;
     }
+
     return $args;
 }, 999, 1);
